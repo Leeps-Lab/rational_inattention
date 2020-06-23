de306a2d85974dd1f75fbd75a15ae4b37ff87770
Melanie <melywong@ucsc.edu>
2020-06-23 13:09:41 -0700

    screen steps test

--- static/rational_inattention/rational_inattention.js
+++ static/rational_inattention/rational_inattention.js
@@ -1,13 +1,17 @@
 import { html, PolymerElement } from '/static/otree-redwood/node_modules/@polymer/polymer/polymer-element.js';
-import './pie-chart.js';
+import './pie_chart.js';
 
 class RationalInattention extends PolymerElement {
 
     static get properties() {
         return {
+            step: {
+                type: Number,
+                value: 0,
+            },
             defaultProb: {
                 type: Number,
-            }
+            },
         }
     }
     static get template() {
@@ -18,11 +22,33 @@ class RationalInattention extends PolymerElement {
             <pie-chart
                 default-prob="[[ defaultProb ]]"
             ></pie-chart>
+            <button type="button" on-click="_nextStep">next</button>
+            <div>Step [[ step ]]</div>
+            <div>
+            <template is="dom-if" if="{{  _showStep(step, 1) }}">
+                <h1>should display as step 1</h1>
+                <div> [[ step ]]</div>
+            </template>
+            </div>
+            <div>
+            <template is="dom-if" if="{{ _showStep(step, 2) }}">
+                <div> [[ step ]]</div>
+            </template>
+            </div>
         `;
     }
 
     _getNondefault(def) {
         return 100 - def;
+    }
+
+    _nextStep() {
+        this.step++;
+    }
+
+    _showStep(step, num) {
+        console.log('chcking step', step, num, step >=num);
+        return step >= num;
     }
 }
 
