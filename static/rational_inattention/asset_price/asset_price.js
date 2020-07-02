e7fb6038b6b94c947cc04cb8b2229bfbb8e9330a
Melanie <melywong@ucsc.edu>
2020-07-02 15:42:46 -0700

    random range for m

--- static/rational_inattention/asset_price/asset_price.js
+++ static/rational_inattention/asset_price/asset_price.js
@@ -5,18 +5,20 @@ class AssetPrice extends PolymerElement {
 
     static get properties() {
         return {
-            // TODO: get input from Python side, set non-neg constraints
             m: {
                 type: Number,
-                value: 0.5,
+            },
+            r: {
+                type: Number,
+                computed: '_getRandomRange(precision)',
             },
             mLow: {
                 type: Number,
-                computed: '_getMLow(m, precision)'
+                computed: '_getMLow(m, precision, r)'
             },
             mHigh: {
                 type: Number,
-                computed: '_getMHigh(m, precision)'
+                computed: '_getMHigh(m, precision, r)'
             },
             precision: {
                 type: Number,
@@ -46,38 +48,67 @@ class AssetPrice extends PolymerElement {
             }
         </style>
         <div>
-            <h3>Your private information about m: [[ _getMLow(m, precision) ]] < m < [[ _getMHigh(m, precision) ]]</h3>
+            <h3>Your private information about m: [[ mLow ]] < m < [[ mHigh ]]</h3>
             <p>Select the price for which you'd like to buy the asset and the price for which you'd like to sell the asset.</p>
             <p>Assuming you don't care about uncertainty, you would expect:</p>
             <p>VL = <span class="non-def">[[ _getNondefault(defaultProb) ]]%</span> * 100 + <span class="def">[[ defaultProb ]]%</span>
-            * [[ _getMLow(m, precision) ]] * 100 = [[ lowValue ]]</p>
+            * [[ mLow ]] * 100 = [[ lowValue ]]</p>
             <p>VH = <span class="non-def">[[ _getNondefault(defaultProb) ]]%</span> * 100 + <span class="def">[[ defaultProb ]]%</span>
-            * [[ _getMHigh(m, precision) ]] *  100 = [[ highValue ]]</p>
+            * [[ mHigh ]] *  100 = [[ highValue ]]</p>
             <asset-slider
                 m="[[ m ]]"
                 precision="[[ precision ]]"
-                high-value="{{ highValue }}"
-                low-value="{{ lowValue }}"
+                high-value="[[ highValue ]]"
+                low-value="[[ lowValue ]]"
+                buy-price="[[ lowValue ]]"
+                sell-price="[[ highValue ]]"
             ></asset-slider>
         </div>
         `;
     }
 
-    _getMHigh(m, precision) {
-        return +((m + (precision / 2)).toFixed(2));
+    _getRandomRange(precision) {
+        let r = Math.random();
+        // between 0 and precision
+        let random = +((r * (precision - 0.01)).toFixed(2));
+        return random;
     }
 
-    _getMLow(m, precision) {
-        return +((m - (precision / 2)).toFixed(2));
+    _getMLowOverflow() {
+        let mLow = this.m / 100 - (this.precision - this.r);
+        if (mLow < 0) {
+            let overflow = 0 - mLow;
+            return overflow;
+        }
+        return 0;
+    }
+    
+    _getMHighOverflow() {
+        let mHigh = this.m / 100 + this.r;
+        if (mHigh > 1) {
+            let overflow = mHigh - 1;
+            return overflow;
+        }
+        return 0;
+    }
+
+    _getMHigh(m, precision, r) {
+        let mHigh = Math.min(1, m / 100 + this.r + this._getMLowOverflow());
+        return +((mHigh).toFixed(2));
+    }
+
+    _getMLow(m, precision, r) {
+        let mLow = Math.max(0, m / 100 - (precision - this.r) - this._getMHighOverflow());
+        return +((mLow).toFixed(2));
     }
 
     _getHighValue(m, defaultProb, precision) {
-        return +((this._getNondefault(defaultProb) + defaultProb * this._getMHigh(m, precision)).toFixed(2));
+        return +((this._getNondefault(defaultProb) + defaultProb * this.mHigh).toFixed(2));
 
     }
 
     _getLowValue(m, defaultProb, precision) {
-        return +((this._getNondefault(defaultProb) + defaultProb * this._getMLow(m, precision)).toFixed(2));
+        return +((this._getNondefault(defaultProb) + defaultProb * this.mLow).toFixed(2));
     }
 
     _getNondefault(def) {
