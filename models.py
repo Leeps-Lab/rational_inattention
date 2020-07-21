82b42f188e7c309ae97eba17b8a33f90820ecf58
Leqi Zeng <51837095+LuckyLeqi@users.noreply.github.com>
2020-07-21 07:40:56 -0700

    Constants Read from Config_file.csv and Random Variables are written into Data.csv
    Constants such as round_num, endowment, and bonds are now read from the config file.
    Create and write a data file that contains new randomized variables for g,m,q,y if randomize_data is true in config_file.csv.
    Reads from data file into variables g,m,q,y

--- models.py
+++ models.py
@@ -23,42 +23,65 @@ Your app description
 class Constants(BaseConstants):
     name_in_url = 'rational_inattention'
     players_per_group = None
-    num_rounds = 3
-    endowment = 100
-    initial_bonds = 1
+    g = []
+    m = []
+    y = []
+    q = []
 
+# Couldn't figure out how to retrieve current round number into subsession
+    temporary_round_number = 0
+
+#reads straight from the config file constants
+    with open('rational_inattention/configs/config_file.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            num_rounds = int(row['num_rounds'])
+            endowment = int(row['endowment'])
+            initial_bonds = int(row['initial_bonds'])
+            buy_option = True if row['buy_option'] == 'True' else False
+            sell_option = True if row['sell_option'] == 'True' else False
+            randomize_data = True if row['randomize_data'] == 'True' else False
+# script to create a csv document with uniformly generated values for each variable
+    if(randomize_data):
+        with open('rational_inattention/configs/data.csv', 'w', newline='') as csvfile:
+            fieldnames = ['num_rounds', 'g' , 'm', 'y', 'q']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for i in range(1 , num_rounds + 1):
+                    writer.writerow(dict([
+                    ('num_rounds',i),
+                    ('g', int(random.uniform(0, 100))),
+                    ('m', int(random.uniform(0, 100))),
+                    ('y', int(random.uniform(0, 100))),
+                    ('q', int(random.uniform(0, 100))),
+                    ]))
+# reads from data.csv into lists
+    with open('rational_inattention/data.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            g.append(
+                int(row['g'])
+            )
+            m.append(
+                int(row['m'])
+            )
+            y.append(
+                int(row['y'])
+            )
+            q.append(
+                int(row['q'])
+            )
     def get_round_number(self):
         return self.round_number
 
-def parse_config(config_file):
-    with open('rational_attention/configs/' + config_file) as f:
-        rows = list(csv.DictReader(f))
-
-        rounds = []
-        for row in rows:
-            rounds.append({
-                'num_rounds': int(row['num_rounds']),
-                'endowment': int(row['endowment']),
-                'initial_bonds': int(row['initial_bonds']),
-                'buy_option': True if row['buy_option'] == 'TRUE' else False,
-                'sell_option': True if row['sell_option'] == 'TRUE' else False,
-            })
-        return rounds
-
 class Subsession(BaseSubsession):
     # initial values of fields for players for each subsession
-    g = models.IntegerField(initial=int(random.uniform(0, 100)))
-    m = models.IntegerField(initial=int(random.uniform(0, 100)))
-    y = models.IntegerField(initial=int(random.uniform(0, 100)))
-    q = models.IntegerField(initial=int(random.uniform(0, 100)))
-    finish = models.BooleanField(initial=False)
 
-    @property
-    def config(self):
-        try:
-            return parse_config(self.session.config['config_file'])[self.round_number-1]
-        except IndexError:
-            return None
+    g = models.IntegerField(initial = Constants.g[ Constants.temporary_round_number ])
+    m = models.IntegerField(initial = Constants.m[ Constants.temporary_round_number ])
+    y = models.IntegerField(initial = Constants.y[ Constants.temporary_round_number ])
+    q = models.IntegerField(initial = Constants.q[ Constants.temporary_round_number ])
+    finish = models.BooleanField(initial=False)
 
 
 class Group(DecisionGroup):
