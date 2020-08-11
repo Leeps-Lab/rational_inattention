126ee557ae462c4f361e58a03aad6c3f95e396b5
Melanie <melywong@ucsc.edu>
2020-08-11 14:17:51 -0700

    add hidden form fields

--- pages.py
+++ pages.py
@@ -1,31 +1,45 @@
 from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
-from .models import Player
+from .models import Group
+from .models import parse_config
 
 
 class MainPage(Page):
+    form_model = 'player'
+    form_fields = ['precision', 'buy_price', 'sell_price']
+
+    def is_displayed(self):
+        return self.subsession.config is not None
+
     def vars_for_template(self):
+        """
+        groups = self.subsession.get_groups()
+        for group in groups:
+            decisions = group.get_group_decisions_events()
+            print("DECISIONS", decisions)
+        """
         return {
             'round_num': self.round_number,
-            'endowment': Constants.endowment,
-            'initial_bonds' : Constants.initial_bonds,
-            'g': self.player.g,
-            'm': self.player.m,
-            'y': self.player.y,
-            'q': self.player.q,
-            'finish': self.player.finish,
-
+            # 'endowment': 100,
+            'g': self.subsession.g,
+            'm': self.subsession.m,
+            'y': self.subsession.y,
+            'q': self.subsession.q,
+            'step': self.subsession.step,
+            'finish': self.subsession.finish,
         }
-    def get_round_number(self):
-        return self.round_number
 
 class ResultsWaitPage(WaitPage):
     pass
 
 
 class Results(Page):
-    pass
+    def vars_for_template(self):
+            groups = self.subsession.get_groups()
+            print('pages groups', groups)
+            for group in groups:
+                decisions = group.get_group_decisions_events()
+                print("DECISIONS", decisions)
 
-
-page_sequence = [MainPage] #, ResultsWaitPage, Results]
+page_sequence = [MainPage, Results]
